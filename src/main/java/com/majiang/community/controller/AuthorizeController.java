0552c56e64261fff583f30f26ca1a142fcb1e9a2
jway <948766581@qq.com>
2020-03-03 22:34:48 +0800

    extract github config from code to properties

--- src/main/java/com/majiang/community/controller/AuthorizeController.java
+++ src/main/java/com/majiang/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.majiang.community.dto.AccessTokenDTO;
 import com.majiang.community.dto.GithubUser;
 import com.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubProvider gitHubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
     /**
      * 接受码匠社区登录首次访问gitHubAPI返回的code值
      * @param code
@@ -22,14 +30,15 @@ public class AuthorizeController {
     public String callBack(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("d3c547072be67a1eb3ba");
-        accessTokenDTO.setClient_secret("099ee401c39310d21a1f08e9e6e827ea54cc6fdd");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = gitHubProvider.getUser(accessToken);
-        System.out.println(user);
+        System.out.println(user.getName());
+
         return "index";
     }
 }
