72fad36cf9e855f24170a76dbcc675fda29fcde9
jway <948766581@qq.com>
2020-03-07 08:19:38 +0800

    not done

--- src/main/java/com/majiang/community/mapper/UserMapper.java
+++ src/main/java/com/majiang/community/mapper/UserMapper.java
@@ -3,9 +3,14 @@ package com.majiang.community.mapper;
 import com.majiang.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+    @Select("select * from user where token = #{token}")
+//    如果形参里面是类的形参，不用写@Param,否则需要写@Param
+    User findUserByToken(@Param("token")String token);
 }
